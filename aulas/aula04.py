18c6e671fe95a977f68675aeb6930a056bdd5763
Allan Souza Lima <contato.allanlima123@gmail.com>
2026-02-22 22:11:38 -0300

    Vendo primeiros comandos do python

---
+++ aulas/aula04.py
@@ -0,0 +1,35 @@
+##Nesta aula, vamos dar nossos primeiros comandos em python.
+
+#Para exibir algo para o usuario, usamos o comando print
+#Toda função deve ter parenteses
+#Toda mensagem deve ser exibida através de aspas simples
+print('Olá, mundo!')
+
+#Além de mensagens, podemos mostrar números e contas
+#Para exibir numeros e resultados de contas, não utilizamos aspas
+print(7)
+
+print(7+3)
+
+
+#Agora vamos juntar duas mensagens
+print('7'+'3')
+
+#Como usamos aspas, aqui não está somando os numeros, e sim juntando as mensagens.
+#Podemos fazer essa mesma ação de juntas mensagens com o + ou com virgula, assim:
+
+print('7','3')
+
+
+#Agora, vamos trabalhar com variaveis. Toda variavel é um objeto. damos o nome e colocamos
+#o sinal de = logo após o nome, que significa recebe, por exemplo:
+#nome = Allan | nome recebe Allan
+
+nome = 'ALLAN'
+idade = 23
+peso = 80
+
+#Agora, vamos mostrar as 3 variaveis em um print apenas
+print(nome, idade, peso)
+
+
